9594b8195d60f01efbc723337677ed23c6b662d6
Øyvind Matheson Wergeland <oyvind@wergeland.org>
2024-07-04 08:41:40 +0200

    Service to set property value.

--- custom_components/connectlife/sensor.py
+++ custom_components/connectlife/sensor.py
@@ -1,11 +1,12 @@
 """Provides a sensor for ConnectLife."""
 
 import logging
-
+import voluptuous as vol
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant, ServiceCall, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers import config_validation as cv, entity_platform, service
 
 from .const import (
     DOMAIN,
@@ -13,6 +14,8 @@ from .const import (
 from .coordinator import ConnectLifeCoordinator
 from .entity import ConnectLifeEntity
 from connectlife.appliance import ConnectLifeAppliance
+
+SERVICE_SET_VALUE = "set_value"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -23,13 +26,18 @@ async def async_setup_entry(
         async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up ConnectLife sensors."""
-
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
-
     for appliance in coordinator.appliances.values():
         async_add_entities(
             ConnectLifeStatusSensor(coordinator, appliance, s) for s in appliance.status_list
         )
+
+    platform = entity_platform.async_get_current_platform()
+    platform.async_register_entity_service(
+        SERVICE_SET_VALUE,
+        {vol.Required("value"): cv.positive_int},
+        "async_set_value",
+    )
 
 
 class ConnectLifeStatusSensor(ConnectLifeEntity, SensorEntity):
@@ -46,7 +54,8 @@ class ConnectLifeStatusSensor(ConnectLifeEntity, SensorEntity):
 
     @callback
     def update_state(self):
-        self._attr_native_value = self.coordinator.appliances[self.device_id].status_list[self.status]
+        if self.status in self.coordinator.appliances[self.device_id].status_list:
+            self._attr_native_value = self.coordinator.appliances[self.device_id].status_list[self.status]
         self._attr_available = self.coordinator.appliances[self.device_id]._offline_state == 1
 
     @callback
@@ -54,3 +63,8 @@ class ConnectLifeStatusSensor(ConnectLifeEntity, SensorEntity):
         """Handle updated data from the coordinator."""
         self.update_state()
         self.async_write_ha_state()
+
+    async def async_set_value(self, value: int) -> None:
+        """Set value for this sensor."""
+        _LOGGER.debug("Setting %s to %d", self.status, value)
+        await self.coordinator.api.update_appliance(self.device_id, {self.status: str(value)})
