oyvindwe/connectlife-ha

commit 07965ee9bebf3d0fea50fdb16baec59ea09060c7

Refactor update device (#57)
- Use coordinator.data
- Update coordinator.data and notify listeners after api.update_appliance

custom_components/connectlife/number.py
@@ -26,7 +26,7 @@ async def async_setup_entry(
 ) -> None:
     """Set up ConnectLife number entities."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
-    for appliance in coordinator.appliances.values():
+    for appliance in coordinator.data.values():
         dictionary = Dictionaries.get_dictionary(appliance)
         async_add_entities(
             ConnectLifeNumberEntity(coordinator, appliance, s, dictionary[s])
@@ -51,15 +51,14 @@ class ConnectLifeNumberEntity(ConnectLifeEntity, NumberEntity):
         self._attr_unique_id = f"{appliance.device_id}-{status}"
         self.status = status
         device_class = dd_entry.number.device_class
-        options = None
         self.entity_description = NumberEntityDescription(
             key=self._attr_unique_id,
             device_class=device_class,
             entity_registry_visible_default=not dd_entry.hide,
             icon=dd_entry.icon,
             name=status.replace("_", " "),
-            native_max_value = dd_entry.number.max_value,
-            native_min_value = dd_entry.number.min_value,
+            native_max_value=dd_entry.number.max_value,
+            native_min_value=dd_entry.number.min_value,
             native_unit_of_measurement=dd_entry.number.unit,
             translation_key=status,
         )
@@ -67,18 +66,16 @@ class ConnectLifeNumberEntity(ConnectLifeEntity, NumberEntity):
 
     @callback
     def update_state(self):
-        if self.status in self.coordinator.appliances[self.device_id].status_list:
-            value = self.coordinator.appliances[self.device_id].status_list[self.status]
+        if self.status in self.coordinator.data[self.device_id].status_list:
+            value = self.coordinator.data[self.device_id].status_list[self.status]
             self._attr_native_value = value
-        self._attr_available = self.coordinator.appliances[self.device_id].offline_state == 1
+        self._attr_available = self.coordinator.data[self.device_id].offline_state == 1
 
     async def async_set_native_value(self, value: float) -> None:
         """Update the current value."""
         value = int(value)
         _LOGGER.debug("Setting %s to %d on %s", self.status, value, self.nickname)
         try:
-            await self.coordinator.api.update_appliance(self.puid, {self.status: str(value)})
+            await self.async_update_device({self.status, value})
         except LifeConnectError as api_error:
             raise ServiceValidationError(str(api_error)) from api_error
-        self._attr_native_value = value
-        self.async_write_ha_state()

custom_components/connectlife/binary_sensor.py
@@ -27,7 +27,7 @@ async def async_setup_entry(
 ) -> None:
     """Set up ConnectLife sensors."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
-    for appliance in coordinator.appliances.values():
+    for appliance in coordinator.data.values():
         dictionary = Dictionaries.get_dictionary(appliance)
         async_add_entities(
             ConnectLifeBinaryStatusSensor(coordinator, appliance, s, dictionary[s])
@@ -61,11 +61,11 @@ class ConnectLifeBinaryStatusSensor(ConnectLifeEntity, BinarySensorEntity):
 
     @callback
     def update_state(self):
-        if self.status in self.coordinator.appliances[self.device_id].status_list:
-            value = self.coordinator.appliances[self.device_id].status_list[self.status]
+        if self.status in self.coordinator.data[self.device_id].status_list:
+            value = self.coordinator.data[self.device_id].status_list[self.status]
             if value in VALUES:
                 self._attr_is_on = VALUES[value]
             else:
                 self._attr_is_on = None
                 _LOGGER.warning("Unknown value %d for %s", value, self.status)
-        self._attr_available = self.coordinator.appliances[self.device_id].offline_state == 1
+        self._attr_available = self.coordinator.data[self.device_id].offline_state == 1

custom_components/connectlife/climate.py
@@ -41,7 +41,7 @@ async def async_setup_entry(
     """Set up ConnectLife sensors."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
     entities = []
-    for appliance in coordinator.appliances.values():
+    for appliance in coordinator.data.values():
         dictionary = Dictionaries.get_dictionary(appliance)
         if is_climate(dictionary):
             entities.append(ConnectLifeClimate(coordinator, appliance, dictionary))
@@ -70,7 +70,6 @@ class ConnectLifeClimate(ConnectLifeEntity, ClimateEntity):
     hvac_action_map: dict[int, HVACAction]
     hvac_mode_map: dict[int, HVACMode]
     hvac_mode_reverse_map: dict[HVACMode, int]
-    mapped_hvac_mode: HVACMode | None
     swing_mode_map: dict[int, str]
     swing_mode_reverse_map: dict[str, int]
     temperature_unit_map: dict[int, UnitOfTemperature]
@@ -99,7 +98,6 @@ class ConnectLifeClimate(ConnectLifeEntity, ClimateEntity):
         self.hvac_action_map = {}
         self.hvac_mode_map = {}
         self.hvac_mode_reverse_map = {}
-        self.mapped_hvac_mode = None
         self.swing_mode_map = {}
         self.swing_mode_reverse_map = {}
         self.temperature_unit_map = {}
@@ -165,7 +163,6 @@ class ConnectLifeClimate(ConnectLifeEntity, ClimateEntity):
             hvac_modes.append(HVACMode.AUTO)
             if IS_ON not in self.target_map:
                 self._attr_hvac_mode = HVACMode.AUTO
-                self.mapped_hvac_mode = HVACMode.AUTO
         self._attr_hvac_modes = hvac_modes
 
         if TEMPERATURE_UNIT not in self.target_map:
@@ -179,20 +176,20 @@ class ConnectLifeClimate(ConnectLifeEntity, ClimateEntity):
     @callback
     def update_state(self) -> None:
         is_on = True
+        hvac_mode = HVACMode.AUTO
         for target, status in self.target_map.items():
-            if status in self.coordinator.appliances[self.device_id].status_list:
-                value = self.coordinator.appliances[self.device_id].status_list[status]
+            if status in self.coordinator.data[self.device_id].status_list:
+                value = self.coordinator.data[self.device_id].status_list[status]
                 if target == IS_ON:
                     # TODO: Support value mapping
                     if value == 0:
                         is_on = False
                 elif target == HVAC_MODE:
                     if value in self.hvac_mode_map:
-                        self.mapped_hvac_mode = self.hvac_mode_map[value]
+                        hvac_mode = self.hvac_mode_map[value]
                     else:
                         # Map to None without warning as we cannot add custom HVAC modes.
-                        # TODO: Or map to auto?
-                        self.mapped_hvac_mode = None
+                        hvac_mode = None
                 elif target == HVAC_ACTION:
                     if value in self.hvac_action_map:
                         self._attr_hvac_action = self.hvac_action_map[value]
@@ -225,8 +222,8 @@ class ConnectLifeClimate(ConnectLifeEntity, ClimateEntity):
                         value = None
                     setattr(self, f"_attr_{target}", value)
 
-        self._attr_hvac_mode = self.mapped_hvac_mode if is_on else HVACMode.OFF
-        self._attr_available = self.coordinator.appliances[self.device_id].offline_state == 1
+        self._attr_hvac_mode = hvac_mode if is_on else HVACMode.OFF
+        self._attr_available = self.coordinator.data[self.device_id].offline_state == 1
 
     def get_temperature_limit(self, temperature_map: [UnitOfTemperature, int]):
         if temperature_map and self._attr_temperature_unit in temperature_map:
@@ -236,39 +233,24 @@ class ConnectLifeClimate(ConnectLifeEntity, ClimateEntity):
 
     async def async_set_humidity(self, humidity):
         """Set new target humidity."""
-        humidity = round(humidity)
-        await self.coordinator.api.update_appliance(self.puid, {self.target_map[TARGET_HUMIDITY]: humidity})
-        self._attr_target_humidity = humidity
-        self.async_write_ha_state()
+        await self.async_update_device({self.target_map[TARGET_HUMIDITY]: round(humidity)})
 
     async def async_set_temperature(self, **kwargs) -> None:
         """Set new target temperature."""
-        if ATTR_TEMPERATURE in kwargs and TARGET_TEMPERATURE in self.target_map:
-            target_temperature = round(kwargs[ATTR_TEMPERATURE])
-            await self.coordinator.api.update_appliance(
-                self.puid,
-                {
-                    self.target_map[TARGET_TEMPERATURE]:
-                        target_temperature
-                }
-            )
-            self._attr_target_temperature = target_temperature
-            self.async_write_ha_state()
+        if ATTR_TEMPERATURE in kwargs:
+            await self.async_update_device({
+                self.target_map[TARGET_TEMPERATURE]: round(kwargs[ATTR_TEMPERATURE])
+            })
 
     async def async_turn_on(self):
         """Turn the entity on."""
         # TODO: Support value mapping
-        await self.coordinator.api.update_appliance(self.puid, {self.target_map[IS_ON]: 1})
-        # Restore hvac_mode
-        self._attr_hvac_mode = self.mapped_hvac_mode
-        self.async_write_ha_state()
+        await self.async_update_device({self.target_map[IS_ON]: 1})
 
     async def async_turn_off(self):
         """Turn the entity off."""
         # TODO: Support value mapping
-        await self.coordinator.api.update_appliance(self.puid, {self.target_map[IS_ON]: 0})
-        self._attr_hvac_mode = HVACMode.OFF
-        self.async_write_ha_state()
+        await self.async_update_device({self.target_map[IS_ON]: 0})
 
     async def async_set_hvac_mode(self, hvac_mode: HVACMode) -> None:
         """Set the HVAC mode."""
@@ -276,27 +258,21 @@ class ConnectLifeClimate(ConnectLifeEntity, ClimateEntity):
             await self.async_turn_off()
         else:
             request = {}
-            if IS_ON in self.target_map:
+            if self._attr_supported_features & ClimateEntityFeature.TURN_ON:
                 # TODO: Support value mapping
                 request[self.target_map[IS_ON]] = 1
             if HVAC_MODE in self.target_map:
                 request[self.target_map[HVAC_MODE]] = self.hvac_mode_reverse_map[hvac_mode]
-            await self.coordinator.api.update_appliance(self.puid, request)
-            self._attr_hvac_mode = hvac_mode
-            self.async_write_ha_state()
+            await self.async_update_device(request)
 
     async def async_set_fan_mode(self, fan_mode: str) -> None:
         """Set the fan mode."""
-        await self.coordinator.api.update_appliance(self.puid, {
+        await self.async_update_device({
             self.target_map[FAN_MODE]: self.fan_mode_reverse_map[fan_mode]
         })
-        self._attr_fan_mode = fan_mode
-        self.async_write_ha_state()
 
     async def async_set_swing_mode(self, swing_mode: str) -> None:
         """Set the swing mode."""
-        await self.coordinator.api.update_appliance(self.puid, {
+        await self.async_update_device({
             self.target_map[SWING_MODE]: self.swing_mode_reverse_map[swing_mode]
         })
-        self._attr_swing_mode = swing_mode
-        self.async_write_ha_state()

custom_components/connectlife/coordinator.py
@@ -11,10 +11,9 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-class ConnectLifeCoordinator(DataUpdateCoordinator):
-    """ConnectLife coordinator."""
 
-    _appliances: dict[str, ConnectLifeAppliance] = {}
+class ConnectLifeCoordinator(DataUpdateCoordinator[dict[str, ConnectLifeAppliance]]):
+    """ConnectLife coordinator."""
 
     def __init__(self, hass, api: ConnectLifeApi):
         """Initialize coordinator."""
@@ -33,7 +32,7 @@ class ConnectLifeCoordinator(DataUpdateCoordinator):
             # handled by the data update coordinator.
             async with async_timeout.timeout(10):
                 await self.api.get_appliances()
-                self._appliances = {a.device_id: a for a in self.api.appliances}
+                return {a.device_id: a for a in self.api.appliances}
         except LifeConnectAuthError as err:
             # Raising ConfigEntryAuthFailed will cancel future updates
             # and start a config flow with SOURCE_REAUTH (async_step_reauth)
@@ -41,7 +40,8 @@ class ConnectLifeCoordinator(DataUpdateCoordinator):
         except LifeConnectError as err:
             raise UpdateFailed(f"Error communicating with API: {err}")
 
-    @property
-    def appliances(self) -> dict[str, ConnectLifeAppliance]:
-        """Dictionary of device id, appliance."""
-        return self._appliances
+    async def async_update_device(self, device_id: str, properties: dict[str, int | str]):
+        """Updates the device, and sets the same data in local copy and notify to avoid refetching."""
+        await self.api.update_appliance(self.data[device_id].puid, {k: str(v) for k, v in properties.items()})
+        self.data[device_id].status_list.update(properties)
+        self.async_update_listeners()

custom_components/connectlife/entity.py
@@ -23,7 +23,6 @@ class ConnectLifeEntity(CoordinatorEntity[ConnectLifeCoordinator]):
         """Initialize the entity."""
         super().__init__(coordinator)
         self.device_id = appliance.device_id
-        self.puid = appliance.puid
         self.nickname = appliance.device_nickname
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, appliance.device_id)},
@@ -42,3 +41,6 @@ class ConnectLifeEntity(CoordinatorEntity[ConnectLifeCoordinator]):
         """Handle updated data from the coordinator."""
         self.update_state()
         self.async_write_ha_state()
+
+    async def async_update_device(self, properties: dict[str, int | str]):
+        await self.coordinator.async_update_device(self.device_id, properties)

custom_components/connectlife/humidifier.py
@@ -35,7 +35,7 @@ async def async_setup_entry(
     """Set up ConnectLife sensors."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
     entities = []
-    for appliance in coordinator.appliances.values():
+    for appliance in coordinator.data.values():
         dictionary = Dictionaries.get_dictionary(appliance)
         if is_humidifier(dictionary):
             entities.append(ConnectLifeHumidifier(coordinator, appliance, dictionary))
@@ -114,8 +114,8 @@ class ConnectLifeHumidifier(ConnectLifeEntity, HumidifierEntity):
     @callback
     def update_state(self) -> None:
         for target, status in self.target_map.items():
-            if status in self.coordinator.appliances[self.device_id].status_list:
-                value = self.coordinator.appliances[self.device_id].status_list[status]
+            if status in self.coordinator.data[self.device_id].status_list:
+                value = self.coordinator.data[self.device_id].status_list[status]
                 if target == IS_ON:
                     # TODO: Support value mapping
                     self._attr_is_on = value == 1
@@ -133,14 +133,11 @@ class ConnectLifeHumidifier(ConnectLifeEntity, HumidifierEntity):
                         _LOGGER.warning("Got unexpected value %d for %s (%s)", value, status, self.nickname)
                 else:
                     setattr(self, f"_attr_{target}", value)
-        self._attr_available = self.coordinator.appliances[self.device_id].offline_state == 1
+        self._attr_available = self.coordinator.data[self.device_id].offline_state == 1
 
     async def async_set_humidity(self, humidity):
         """Set new target humidity."""
-        humidity = round(humidity)
-        await self.coordinator.api.update_appliance(self.puid, {self.target_map[TARGET_HUMIDITY]: humidity})
-        self._attr_target_humidity = humidity
-        self.async_write_ha_state()
+        await self.async_update_device({self.target_map[TARGET_HUMIDITY]: round(humidity)})
 
     async def async_turn_on(self):
         """Turn the entity on."""
@@ -148,9 +145,7 @@ class ConnectLifeHumidifier(ConnectLifeEntity, HumidifierEntity):
             _LOGGER.warning("Cannot turn on %s without is_on target.", self.nickname)
             return
         # TODO: Support value mapping
-        await self.coordinator.api.update_appliance(self.puid, {self.target_map[IS_ON]: 1})
-        self._attr_is_on = True
-        self.async_write_ha_state()
+        await self.async_update_device({self.target_map[IS_ON]: 1})
 
     async def async_turn_off(self):
         """Turn the entity off."""
@@ -158,14 +153,8 @@ class ConnectLifeHumidifier(ConnectLifeEntity, HumidifierEntity):
             _LOGGER.warning("Cannot turn off %s without is_on target.", self.nickname)
             return
         # TODO: Support value mapping
-        await self.coordinator.api.update_appliance(self.puid, {self.target_map[IS_ON]: 0})
-        self._attr_is_on = False
-        self.async_write_ha_state()
+        await self.async_update_device({self.target_map[IS_ON]: 0})
 
     async def async_set_mode(self, mode):
         """Set mode."""
-        await self.coordinator.api.update_appliance(self.puid, {
-            self.target_map[MODE]: self.mode_reverse_map[mode]
-        })
-        self._attr_mode = mode
-        self.async_write_ha_state()
+        await self.async_update_device({self.target_map[MODE]: self.mode_reverse_map[mode]})

custom_components/connectlife/select.py
@@ -25,7 +25,7 @@ async def async_setup_entry(
 ) -> None:
     """Set up ConnectLife selectors."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
-    for appliance in coordinator.appliances.values():
+    for appliance in coordinator.data.values():
         dictionary = Dictionaries.get_dictionary(appliance)
         async_add_entities(
             ConnectLifeSelect(coordinator, appliance, s, dictionary[s])
@@ -63,8 +63,8 @@ class ConnectLifeSelect(ConnectLifeEntity, SelectEntity):
 
     @callback
     def update_state(self):
-        if self.status in self.coordinator.appliances[self.device_id].status_list:
-            value = self.coordinator.appliances[self.device_id].status_list[self.status]
+        if self.status in self.coordinator.data[self.device_id].status_list:
+            value = self.coordinator.data[self.device_id].status_list[self.status]
             if value in self.options_map:
                 value = self.options_map[value]
             else:
@@ -74,6 +74,4 @@ class ConnectLifeSelect(ConnectLifeEntity, SelectEntity):
 
     async def async_select_option(self, option: str) -> None:
         """Change the selected option."""
-        await self.coordinator.api.update_appliance(self.puid, {self.status: self.reverse_options_map[option]})
-        self._attr_current_option = option
-        self.async_write_ha_state()
+        await self.async_update_device({self.status: self.reverse_options_map[option]})

custom_components/connectlife/sensor.py
@@ -5,7 +5,7 @@ import voluptuous as vol
 from homeassistant.components.sensor import SensorEntity, SensorStateClass, SensorDeviceClass, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant, ServiceCall, callback
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import ServiceValidationError
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers import config_validation as cv, entity_platform, service
@@ -31,7 +31,7 @@ async def async_setup_entry(
 ) -> None:
     """Set up ConnectLife sensors."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
-    for appliance in coordinator.appliances.values():
+    for appliance in coordinator.data.values():
         dictionary = Dictionaries.get_dictionary(appliance)
         async_add_entities(
             ConnectLifeStatusSensor(coordinator, appliance, s, dictionary[s])
@@ -68,11 +68,11 @@ class ConnectLifeStatusSensor(ConnectLifeEntity, SensorEntity):
             self.options_map = dd_entry.sensor.options
             options = list(self.options_map.values())
         elif (device_class is None
-              and isinstance(self.coordinator.appliances[self.device_id].status_list[status], datetime.datetime)):
+              and isinstance(self.coordinator.data[self.device_id].status_list[status], datetime.datetime)):
             device_class = SensorDeviceClass.TIMESTAMP
         state_class = dd_entry.sensor.state_class
         if (state_class is None
-                and isinstance(self.coordinator.appliances[self.device_id].status_list[status], int)
+                and isinstance(self.coordinator.data[self.device_id].status_list[status], int)
                 and device_class != SensorDeviceClass.ENUM):
             state_class = SensorStateClass.MEASUREMENT
         self.entity_description = SensorEntityDescription(
@@ -90,8 +90,8 @@ class ConnectLifeStatusSensor(ConnectLifeEntity, SensorEntity):
 
     @callback
     def update_state(self):
-        if self.status in self.coordinator.appliances[self.device_id].status_list:
-            value = self.coordinator.appliances[self.device_id].status_list[self.status]
+        if self.status in self.coordinator.data[self.device_id].status_list:
+            value = self.coordinator.data[self.device_id].status_list[self.status]
             if self.device_class == SensorDeviceClass.ENUM:
                 if value in self.options_map:
                     value = self.options_map[value]
@@ -99,7 +99,7 @@ class ConnectLifeStatusSensor(ConnectLifeEntity, SensorEntity):
                     _LOGGER.warning("Got unexpected value %d for %s (%s)", value, self.status, self.nickname)
                     value = None
             self._attr_native_value = value if value != self.unknown_value else None
-        self._attr_available = self.coordinator.appliances[self.device_id].offline_state == 1
+        self._attr_available = self.coordinator.data[self.device_id].offline_state == 1
 
     async def async_set_value(self, value: int) -> None:
         """Set value for this sensor."""
@@ -109,8 +109,6 @@ class ConnectLifeStatusSensor(ConnectLifeEntity, SensorEntity):
         elif self.read_only:
             raise ServiceValidationError(f"{self._attr_name} is read-only on {self.nickname}")
         try:
-            await self.coordinator.api.update_appliance(self.puid, {self.status: str(value)})
+            await self.async_update_device({self.status: value})
         except LifeConnectError as api_error:
             raise ServiceValidationError(str(api_error)) from api_error
-        self._attr_native_value = value
-        self.async_write_ha_state()

commit 80d6113dcc5103e4c4bcaa0c78fe21785072d307

Number entity fixes (#72)
- Fix regression when creating entity (introduced in PR #57)
- Fix regression when setting value (introduced in PR #57)

custom_components/connectlife/number.py
@@ -29,7 +29,7 @@ async def async_setup_entry(
     for appliance in coordinator.data.values():
         dictionary = Dictionaries.get_dictionary(appliance)
         async_add_entities(
-            ConnectLifeNumberEntity(coordinator, appliance, s, dictionary[s])
+            ConnectLifeNumberEntity(coordinator, appliance, s, dictionary.properties[s])
             for s in appliance.status_list if hasattr(dictionary.properties[s], Platform.NUMBER)
         )
 
@@ -76,6 +76,6 @@ class ConnectLifeNumberEntity(ConnectLifeEntity, NumberEntity):
         value = int(value)
         _LOGGER.debug("Setting %s to %d on %s", self.status, value, self.nickname)
         try:
-            await self.async_update_device({self.status, value})
+            await self.async_update_device({self.status: value})
         except LifeConnectError as api_error:
             raise ServiceValidationError(str(api_error)) from api_error