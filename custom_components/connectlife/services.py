e01c70205173332440983613008b26327d2ed2ec
Zrean Tofiq <mote93@gmail.com>
2025-01-21 16:50:22 +0100

    Generic update service (#201)

--- custom_components/connectlife/services.py
+++ custom_components/connectlife/services.py
@@ -16,7 +16,9 @@ from .coordinator import ConnectLifeCoordinator
 from .const import DOMAIN
 
 ATTR_ACTION = "action"
+ATTR_DATA = "data"
 SERVICE_SET_ACTION = "set_action"
+SERVICE_UPDATE = "update"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -25,7 +27,7 @@ async def async_setup_services(hass: HomeAssistant) -> None:
     """Set up the services for the Fully Kiosk Browser integration."""
 
     async def collect_coordinators(
-            device_ids: list[str],
+        device_ids: list[str],
     ) -> dict[str, ConnectLifeCoordinator]:
         config_entries = dict[str, ConfigEntry]()
         registry = dr.async_get(hass)
@@ -36,7 +38,7 @@ async def async_setup_services(hass: HomeAssistant) -> None:
                 for entry_id in device.config_entries:
                     entry = hass.config_entries.async_get_entry(entry_id)
                     if entry and entry.domain == DOMAIN:
-                        for (domain, device_id) in device.identifiers:
+                        for domain, device_id in device.identifiers:
                             if domain == DOMAIN:
                                 _LOGGER.debug(f"device_id: {device_id}")
                                 device_entries[device_id] = entry
@@ -57,14 +59,20 @@ async def async_setup_services(hass: HomeAssistant) -> None:
             coordinators[device_id] = hass.data[DOMAIN][config_entry.entry_id]
         return coordinators
 
-    async def async_set_action(call: ServiceCall) -> None:
-        """Set action on device."""
-        coordinators = await collect_coordinators(call.data[ATTR_DEVICE_ID])
+    async def _async_update(devices: list[str], data: dict[str, any]) -> None:
+        """Update properties on device."""
+        coordinators = await collect_coordinators(devices)
         for device_id, coordinator in coordinators.items():
-            _LOGGER.debug("Setting Actions to %d on %s", call.data[ATTR_ACTION], device_id)
+            _LOGGER.debug(f"Updating {device_id} with data: {data}")
             # TODO: Consider trigging a data update to avoid waiting for next poll to update state.
             #       Make sure to only do this once per coordinater.
-            await coordinator.async_update_device(device_id, {"Actions":  call.data[ATTR_ACTION]}, {})
+            await coordinator.async_update_device(device_id, data, {})
+
+    async def async_set_action(call: ServiceCall) -> None:
+        """Set action on device."""
+        await _async_update(
+            call.data[ATTR_DEVICE_ID], {"Actions": call.data[ATTR_ACTION]}
+        )
 
     hass.services.async_register(
         DOMAIN,
@@ -75,6 +83,24 @@ async def async_setup_services(hass: HomeAssistant) -> None:
                 {
                     vol.Required(ATTR_DEVICE_ID): cv.ensure_list,
                     vol.Required(ATTR_ACTION): cv.positive_int,
+                }
+            )
+        ),
+    )
+
+    async def async_update(call: ServiceCall) -> None:
+        """Action handler for updating properties on device."""
+        await _async_update(call.data[ATTR_DEVICE_ID], call.data[ATTR_DATA])
+
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_UPDATE,
+        async_update,
+        schema=vol.Schema(
+            vol.All(
+                {
+                    vol.Required(ATTR_DEVICE_ID): cv.ensure_list,
+                    vol.Required(ATTR_DATA): dict,
                 }
             )
         ),
