6664a1a20baebe856d2a415e8fd5821d5e6ee429
Øyvind Matheson Wergeland <oyvind@wergeland.org>
2026-05-14 20:04:24 +0200

    Drop is_entity in favor of has_platform

--- custom_components/connectlife/utils.py
+++ custom_components/connectlife/utils.py
@@ -1,5 +1,3 @@
-import datetime as dt
-
 from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.const import Platform, UnitOfTemperature
 
@@ -12,16 +10,6 @@ def has_platform(platform: Platform, property: Property):
     return hasattr(property, platform) and not property.disable
 
 
-def is_entity(platform: Platform, property: Property, value: float | int | str | dt.datetime | None):
-    return (
-            has_platform(platform, property)
-            and (
-                    property.unavailable is None
-                    or value != property.unavailable
-            )
-    )
-
-
 def to_unit(unit: str | None, appliance: ConnectLifeAppliance, dictionary: Dictionary):
     if unit is None:
         return None
@@ -30,15 +18,15 @@ def to_unit(unit: str | None, appliance: ConnectLifeAppliance, dictionary: Dicti
         if unit_property_name in dictionary.properties:
             unit_value = appliance.status_list[unit_property_name]
             unit_property = dictionary.properties[unit_property_name]
-            if is_entity(Platform.CLIMATE, unit_property, unit_value):
+            if has_platform(Platform.CLIMATE, unit_property):
                 unit_climate = unit_property.climate
                 if unit_climate.target == TEMPERATURE_UNIT and unit_value in unit_climate.options:
                     unit = unit_climate.options[unit_value]
-            elif is_entity(Platform.SENSOR, unit_property, unit_value):
+            elif has_platform(Platform.SENSOR, unit_property):
                 unit_sensor = unit_property.sensor
                 if unit_sensor.device_class == SensorDeviceClass.ENUM and unit_sensor.options is not None and unit_value in unit_sensor.options:
                     unit = unit_sensor.options[unit_value]  # type: ignore[index]
-            elif is_entity(Platform.SELECT, unit_property, unit_value):
+            elif has_platform(Platform.SELECT, unit_property):
                 unit_select = unit_property.select
                 if unit_value in unit_select.options:
                     unit = unit_select.options[unit_value]
